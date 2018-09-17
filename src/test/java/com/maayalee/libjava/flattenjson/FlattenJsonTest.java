cdfe6e165a1d4d5e041849a4a648a6210f16e123
Lee, Hyeon-gi <maaya.lee@gmail.com>
2018-09-18 08:01:01 +0900

    Added unittest assert

--- src/test/java/com/maayalee/libjava/flattenjson/FlattenJsonTest.java
+++ src/test/java/com/maayalee/libjava/flattenjson/FlattenJsonTest.java
@@ -44,22 +44,37 @@ public class FlattenJsonTest extends TestCase {
   }
 
   public void testFlattenArrayOfObject() {
-    String jsonString = "{\"field1\":\"value1\",\"field2\":\"value2\",\"items\":[{\"id\":1,\"item\":\"item1\"},{\"id\":2,\"item\":\"item2\"},{\"id\":3,\"item\":\"item3\",\"childs\":[{\"key1\":1,\"key2\":2},{\"key1\":1,\"key2\":2}]}]}";
+    String jsonString = "{\"field1\":\"value1\",\"field2\":\"value2\",\"items\":[{\"id\":1,\"name\":\"item1\"},{\"id\":2,\"name\":\"item2\"},{\"id\":3,\"name\":\"item3\",\"childs\":[{\"key1\":1,\"key2\":2},{\"key1\":1,\"key2\":2}]}]}";
     Type type = new TypeToken<Map<String, Object>>() {
     }.getType();
     Map<String, Object> element = new Gson().fromJson(jsonString, type);
 
     FlattenJson flatten = new FlattenJson();
-    flatten.addRule("$.items[].id", "extract_rows1.id");
-    flatten.addRule("$.items[].item", "extract_rows1.item");
-    flatten.addRule("$.items[].childs[].key1", "extract_rows2.key1");
-    flatten.addRule("$.items[].childs[].key2", "extract_rows2.key2");
+    flatten.addRule("$.items[].id", "extract_rows1.item_id");
+    flatten.addRule("$.items[].name", "extract_rows1.item_name");
+    flatten.addRule("$.items[].childs[].key1", "extract_rows2.item_child_key1");
+    flatten.addRule("$.items[].childs[].key2", "extract_rows2.item_child_key2");
 
     Map<String, Object> unnestRow = flatten.unnest(element);
+    assertTrue(unnestRow.get("field1").equals("value1"));
+    assertTrue(unnestRow.get("field2").equals("value2"));
+    assertFalse(unnestRow.containsKey("items"));
+
     Map<Integer, Map<String, Object>> extractRows = flatten.getExtractRows("extract_rows1");
     assertTrue(extractRows.size() == 3);
+    assertTrue(extractRows.get(0).get("item_id").equals(1.0));
+    assertTrue(extractRows.get(0).get("item_name").equals("item1"));
+    assertTrue(extractRows.get(1).get("item_id").equals(2.0));
+    assertTrue(extractRows.get(1).get("item_name").equals("item2"));
+    assertTrue(extractRows.get(2).get("item_id").equals(3.0));
+    assertTrue(extractRows.get(2).get("item_name").equals("item3"));
+
     extractRows = flatten.getExtractRows("extract_rows2");
     assertTrue(extractRows.size() == 2);
+    assertTrue(extractRows.get(0).get("item_child_key1").equals(1.0));
+    assertTrue(extractRows.get(0).get("item_child_key2").equals(2.0));
+    assertTrue(extractRows.get(1).get("item_child_key1").equals(1.0));
+    assertTrue(extractRows.get(1).get("item_child_key2").equals(2.0));
   }
 
   public void testFlattenArrayOfValue() {
